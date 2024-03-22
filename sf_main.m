74f9dd4754629fa3e1872bf8bc9389216456104a
Anushka-N12 <anushka.narsima@gmail.com>
2024-03-22 19:50:22 +0400

    Autocorr & beat spec done

--- sf_main.m
+++ sf_main.m
@@ -3,7 +3,7 @@ clear;
 clc;
 
 disp('Starting program');
-filepath = "attention.wav";
+filepath = "The Good Soldier (mp3cut.net).wav";
 [y,Fs_orig] = audioread(filepath);  % Use a different variable to store the original Fs
 disp('Loaded .wav file');
 
@@ -32,38 +32,45 @@ imagesc(T, F, 10*log10(abs(S))); % Plot the spectrogram in dB scale
 axis xy; % Flip the y-axis direction to match the conventional axis orientation
 xlabel('Time (s)');
 ylabel('Frequency (Hz)');
-title('Spectrogram of attention.wav');
+title('Mixed Spectrogram of audio file');
 
 S_size = size(S);
 disp(S_size);
 
 % Slide the rows of the mixture spectrogram
 % Direct call didn't work due to memory limits
-B = zeros(S_size);  % Initialize the autocorrelation matrix
 
 disp('Running autocorr loop...');
 tic;
-% Calculate autocorrelation for each column
-for i = 1:S_size(2)
-    lagged_c = circshift(S(:, i), 1); % Shift by one sample to create a lagged version
 
-    corr_c = xcorr(S(:, i), lagged_c);  % This will compute the autocorrelation of each row
-    B(i, :) = corr_c(1, floor(length(corr_c(1,:))/2)+1:end); % Take the second half only
+% Calculate autocorrelation for first row
+corr_r = xcorr(S(1, :));
+B = zeros(size(corr_r, 2), S_size(1));  % Initialize the autocorrelation matrix
+B(:, 1) = corr_r;
+
+% Calculate autocorrelation for each row
+for i = 2:S_size(1)
+    corr_r = xcorr(S(i, :));
+    B(:, i) = corr_r;
 end
 et = datestr(datenum(0,0,0,0,0,toc), 'MM:SS');
 disp(['Time taken to run autocorr loop - ', et]);
 
-% Compute the mean value for each row/column of matrix B to obtain the beat spectrum b
+% Compute the mean value for each row of matrix B to obtain the beat spectrum b
 bs_r = mean(B, 2); % Compute mean along the rows (dimension 2)
 
-% Normalisation
-bs_r_n = normalize(bs_r);
+% Normalisation using first value
+bs_r_n = bs_r / bs_r(1);
+
+t = 1:size(bs_r_n);
 
 % Plot the final beat spectrum
 figure;
-plot(bs_r);
+plot(t, bs_r_n);
 xlabel('Index');
 ylabel('Value');
 title('Final Beat Spectrum');
+
+
 
 
